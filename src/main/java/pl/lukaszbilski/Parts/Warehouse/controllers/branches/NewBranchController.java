d255c4f29fd648e9968328777d351201f7dd78a4
Lukasz-Bilski <lukaszbilski1@gmail.com>
2019-06-19 20:15:01 +0200

    implement function to add new branch

--- src/main/java/pl/lukaszbilski/Parts/Warehouse/controllers/branches/NewBranchController.java
+++ src/main/java/pl/lukaszbilski/Parts/Warehouse/controllers/branches/NewBranchController.java
@@ -2,7 +2,12 @@ package pl.lukaszbilski.Parts.Warehouse.controllers.branches;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import pl.lukaszbilski.Parts.Warehouse.models.models.CompanyBranchModel;
+import pl.lukaszbilski.Parts.Warehouse.models.repositories.CompanyBranchRepository;
 
 @Controller
 public class NewBranchController {
@@ -10,7 +15,26 @@ public class NewBranchController {
     @FXML
     Button saveAndExit;
 
-    public void saveAndExit() {
+    @FXML
+    TextField loginHart, branchHart, notesHart, loginIC, branchIC, notesIC, loginAP, notesAP, branchName;
 
+    @Autowired
+    CompanyBranchRepository companyBranchRepository;
+
+    public void saveAndExit() {
+        CompanyBranchModel newBranch = new CompanyBranchModel();
+
+        newBranch.setCity(branchName.getText());
+        newBranch.setLogin_Hart(loginHart.getText());
+        newBranch.setBranch_Hart(branchHart.getText());
+        newBranch.setNotes_Hart(notesHart.getText());
+        newBranch.setLogin_Inter_Cars(loginIC.getText());
+        newBranch.setBranch_Inter_Cars(branchIC.getText());
+        newBranch.setNotes_Inter_Cars(notesIC.getText());
+
+        companyBranchRepository.save(newBranch);
+
+        Stage stageToClose = (Stage) saveAndExit.getScene().getWindow();
+        stageToClose.close();
     }
 }
